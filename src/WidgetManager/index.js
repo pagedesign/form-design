16b7da9709bfe501bbfb533784b04cea9940a388
nobo <nobo.zhou@idreamsky.com>
2019-06-01 18:28:55 +0800

    Add        src/Designer.js Add        src/WidgetManager/Widget.js Add        src/WidgetManager/index.js Add        src/panels/DesignPanel.js Add        src/panels/PropertyPanel.js Add        src/panels/WidgetsPanel.js Add        src/panels/index.js

---
+++ src/WidgetManager/index.js
@@ -0,0 +1,34 @@
+import Widget from './Widget';
+
+/**
+ *
+ *
+ * @class WidgetManager
+ */
+class WidgetManager {
+
+    widgets = [];
+
+    /**
+     * 设计器控件注册
+     *
+     * @param {object} widget
+     * @param {object} props
+     * @memberof WidgetManager
+     */
+    register(widget, props) {
+
+    }
+    /**
+     *
+     *
+     * @param {string} xtype
+     * @memberof WidgetManager
+     */
+    unregister(xtype) {
+
+    }
+
+}
+
+export default new WidgetManager();
---
+++ src/panels/index.js
@@ -0,0 +1,9 @@
+import WidgetsPanel from './WidgetsPanel';
+import DesignPanel from './DesignPanel';
+import PropertyPanel from './PropertyPanel';
+
+export {
+    WidgetsPanel,
+    DesignPanel,
+    PropertyPanel,
+}
